ff1c02110d32c66d52e95a60a65110707ecd68df
Venu <mvgr530@gmail.com>
2024-06-13 14:47:20 +0530

    s3 secrets are added

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -6,12 +6,13 @@ function run() {
     // 1. get some input values
     const bucket = core.getInput('bucket', {required: true});
     const bucketRegion = core.getInput('bucket-region', {required: true});
-    const distFolder = core.getInput('dist-folder', required: true);
+    const distFolder = core.getInput('dist-folder', {required: true});
 
     // 2. Upload files
-    const s3Uri= `s3://${bucket}`
-    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`) 
-     
+    const s3Uri= `s3://${bucket}`;
+
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
 
 
 
